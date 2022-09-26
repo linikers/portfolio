54f817f4824e275c821080a8c01f1bf96781b265
linikers <linikers@hotmail.com>
2022-09-26 09:05:50 -0300

    versão 0.06

--- js/alternateColor.js
+++ js/alternateColor.js
@@ -1,16 +1,13 @@
-const colors = ["green", "red", "rgba(133, 122, 200)", "#F15025"];
+const colors    = ["green", "red", "rgba(133, 122, 200)", "#F15025"];
+const btn       = document.getElementById('btn');
+const color     = document.querySelector(".color");
 
-const btn = document.getElementById('btn');
-
-const color = document.querySelector(".cor")
-
-btn.addEventListener('click', function() {
-        
+btn.addEventListener("click", function() {
     //obter numero aleatorio entre 0 e 3
     const aleatorio = obterAleatorio();
-    console.log(aleatorio)
-    document.body.style.backgroundColor = colors[aleatorio]
-    color.textContent = colors[aleatorio]
+    
+    document.body.style.backgroundColor = colors[aleatorio];
+    color.textContent = colors[aleatorio];
 })
 //console.log(document.body)
 //console.log(aleatorio)
