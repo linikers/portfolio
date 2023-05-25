5036c514b58d845faefc9075ac0369e39f391dee
liniker <linikers@hotmail.com>
2023-05-25 18:51:05 -0300

    cores teste

--- linikers/src/components/logo.tsx
+++ linikers/src/components/logo.tsx
@@ -1,10 +1,10 @@
 export default function Logo() {
   return (
-    <figure className="font-source-code-pro text-lg  p-2 m-1 text-center shadow-xl">
-      <span className="text-custom-base-0">Seja Bem Vindo</span>
-      <p className="text-custom-base-1 text-xl mt-2 mb-2">LinikerS</p>
-      <span className="text-custom-blue-1">
-        Front-End developer | E-commerce | SEO
+    <figure className="font-source-code-pro font-bold text-lg  p-2 m-1 text-center shadow-xl">
+      <span className="text-custom-base-1">Seja Bem Vindo</span>
+      <p className="text-custom-base-2 text-xl mt-2 mb-2">LinikerS</p>
+      <span className="text-custom-red-1">
+        Front-End Developer | E-commerce | SEO
       </span>
     </figure>
   );
