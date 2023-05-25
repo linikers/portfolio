79c0d24a2ff0f2cbc3e47b4a7b4126e3bffeb83c
liniker <linikers@hotmail.com>
2023-05-25 11:13:02 -0300

    menu base | logo | background

--- linikers/src/components/logo.tsx
+++ linikers/src/components/logo.tsx
@@ -1,7 +1,9 @@
 export default function Logo() {
   return (
     <figure className="font-source-code-pro text-xl text-custom-red-1 p-2 m-1">
+      <span>Seja Bem Vindo</span>
       <p>LinikerS</p>
+      <span>Front-End developer | SEO | E-commerce</span>
     </figure>
   );
 }
--- linikers/src/pages/index.tsx
+++ linikers/src/pages/index.tsx
@@ -1,16 +1,20 @@
 import Logo from "@/components/logo";
+import MenuUser from "@/components/menu";
 import BoxTop from "@/components/top";
 import { Container } from "reactstrap";
 
 export default function Home() {
   return (
-    <Container className="bg-custom-base-0">
-      <header className="py-4">
+    <Container className="bg-custom-base-0 min-h-screen flex flex-col items-center justify-center">
+      <div className="absolute top-0 right-0 z-10">
+        <MenuUser />
+      </div>
+      <div className="z-10">
         <Logo />
-      </header>
-      <main>
+      </div>
+      <div className="z-0">
         <BoxTop />
-      </main>
+      </div>
     </Container>
   );
 }
