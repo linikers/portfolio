70255d71a3f6b03b146d2efdcf468c705da0628c
liniker <linikers@hotmail.com>
2023-05-26 15:21:12 -0300

    base intro

--- linikers/src/pages/index.tsx
+++ linikers/src/pages/index.tsx
@@ -6,7 +6,7 @@ import { Container } from "reactstrap";
 export default function Home() {
   return (
     <Container className="min-h-screen flex flex-col items-center justify-center">
-      <div className="absolute top-0 right-0 z-10 m-6">
+      <div className="absolute z-10 flex flex-col items-center">
         <MenuUser />
         <div className="z-10 flex flex-col items-center justify-center">
           <Logo />
