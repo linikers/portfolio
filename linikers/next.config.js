de3f557611c12f0f09bf9f28b1eea91c04b1cdbe
liniker <linikers@hotmail.com>
2023-05-29 17:15:39 -0300

    menu color ok

--- linikers/next.config.js
+++ linikers/next.config.js
@@ -2,14 +2,16 @@
 const nextConfig = {
   pageExtensions: ["jsx", "js", "tsx", "ts"],
 };
-
+const path = require("path");
 module.exports = {
   ...nextConfig,
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
-      config.resolve.alias["@/components"] =
-        require.resolve("./src/components");
-      config.resolve.alias["@/styles"] = require.resolve("./src/styles");
+      config.resolve.alias["@/components"] = path.join(
+        __dirname,
+        "src/components"
+      );
+      config.resolve.alias["@/styles"] = path.join(__dirname, "src/styles");
     }
 
     return config;
