e508512d6af67c6fb09ea1c56371b38966491abf
liniker <linikers@hotmail.com>
2023-05-31 11:44:07 -0300

    teste de maquina de escrever

--- linikers/src/components/logo.tsx
+++ linikers/src/components/logo.tsx
@@ -1,23 +1,51 @@
+import { useEffect, useState } from "react";
 import MaquinaDeEscrever from "./maquinaEscrever";
 
 export default function Logo() {
+  const [lines, setLines] = useState<string[]>([
+    "Olá, clique no avatar para saber mais sobre",
+    "LinikerS",
+    "Front-End Developer | E-commerce | SEO",
+  ]);
+  const [currentLine, setCurrentLine] = useState<number>(0);
+
+  useEffect(() => {
+    if (currentLine < lines.length) {
+      const timeoutId = setTimeout(() => {
+        setCurrentLine((prevLine) => prevLine + 1);
+      }, 2000); // Delay entre cada linha (2000ms)
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [currentLine, lines]);
+
   return (
-    <figure className="font-source-code-pro font-bold text-lg  p-2 m-1 text-center shadow-xl">
-      <span className="text-custom-base-1 shadow-xl">
-        <MaquinaDeEscrever
-          lines={["Olá, clique no avatar para saber mais sobre"]}
-          delay={180}
-        />
-      </span>
-      <p className="text-custom-base-2 text-xl mt-2 mb-2 shadow-xl">
-        <MaquinaDeEscrever lines={["LinikerS"]} delay={380} />
-      </p>
-      <span className="text-custom-red-1">
-        <MaquinaDeEscrever
-          lines={["Front-End Developer | E-commerce | SEO"]}
-          delay={480}
-        />
-      </span>
+    <figure className="font-source-code-pro font-bold text-lg p-2 m-1 text-center shadow-xl">
+      {lines.map((line, index) => (
+        <div key={index}>
+          {index < currentLine ? (
+            <>
+              {index === 0 && (
+                <span className="text-custom-base-1 shadow-xl">
+                  <MaquinaDeEscrever lines={[line]} />
+                </span>
+              )}
+              {index === 1 && (
+                <p className="text-custom-base-2 text-xl mt-2 mb-2 shadow-xl">
+                  <MaquinaDeEscrever lines={[line]} />
+                </p>
+              )}
+              {index === 2 && (
+                <span className="text-custom-red-1">
+                  <MaquinaDeEscrever lines={[line]} />
+                </span>
+              )}
+            </>
+          ) : null}
+        </div>
+      ))}
     </figure>
   );
 }
