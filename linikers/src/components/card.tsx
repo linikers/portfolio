81f222ab4e49cb633e68368f334c0c554c59407e
liniker <linikers@hotmail.com>
2023-06-05 16:31:59 -0300

    estilização do card

---
+++ linikers/src/components/card.tsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+} from "reactstrap";
+import Image from "next/image";
+import rickmorty from "../../public/rmtumbl.png";
+import Link from "next/link";
+
+interface iCardPortfilioProps {
+  imgUrl: string;
+  altText: string;
+  title: string;
+  subTitle: string;
+  description: string;
+  url: string;
+}
+export default function CardPortfolio({
+  imgUrl,
+  altText,
+  title,
+  subTitle,
+  description,
+  url,
+}: iCardPortfilioProps) {
+  return (
+    <Card className="m-4 ">
+      <Image src={imgUrl} alt={altText} quality={75} width={280} height={280} />
+      <CardBody className="text-center">
+        <CardTitle tag="h5" className="text-custom-base-2 fw-bold">
+          {title}
+        </CardTitle>
+        <CardSubtitle className="mb-2 text-muted text-custom-base-1" tag="h6">
+          {subTitle}
+        </CardSubtitle>
+        <CardText className="text-custom-base-0">{description}</CardText>
+        <Link href={url}>
+          <Button className="bg-custom-base-1 btn btn-link rounded-xl px-4 py-2">
+            Acessar
+          </Button>
+        </Link>
+      </CardBody>
+    </Card>
+  );
+}
