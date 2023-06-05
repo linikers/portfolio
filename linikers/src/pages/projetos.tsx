e0582d8d4471710c0e59c0fff11efab5344d6ce7
liniker <linikers@hotmail.com>
2023-06-05 16:31:59 -0300

    estilização do card

--- linikers/src/pages/projetos.tsx
+++ linikers/src/pages/projetos.tsx
@@ -5,35 +5,76 @@ import {
   CardSubtitle,
   CardText,
   CardTitle,
+  ListGroup,
 } from "reactstrap";
 import Image from "next/image";
 import rickmorty from "../../public/rmtumbl.png";
+import CardPortfolio from "@/components/card";
 
 export default function Projetos() {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
   return (
-    <Card
-      style={{
-        width: "18rem",
-      }}
-    >
-      <Image
-        src={rickmorty}
-        alt="tumblr1"
-        quality={75}
-        width={300}
-        height={300}
-      />
-      <CardBody>
-        <CardTitle tag="h5">RICK e MORTY</CardTitle>
-        <CardSubtitle className="mb-2 text-muted" tag="h6">
-          Projeto Rick e Morty
-        </CardSubtitle>
-        <CardText>
-          Nesse projeto foi usado Next.Js com Sass utilizando API Rick e Morty
-          com busca e opção de favoritos.
-        </CardText>
-        <Button>Button</Button>
-      </CardBody>
-    </Card>
+    <ListGroup className="flex flex-wrap bg-custom-black-0">
+      <Card
+        style={{
+          width: "18rem",
+        }}
+      >
+        <CardPortfolio
+          imgUrl={`${basePath}/rmtumbl.png`}
+          altText="RICK  and MORTY"
+          title="RB Nail Design"
+          subTitle=" Rafa Bach Nails"
+          description="Nesse Projeto foi usado Next.Js, tailwind, Sass, typescript e bootstrap com reactstrap, utilizando API Rick e Morty
+        com  opção de busca e opção de favoritos."
+          url="https://rick-morty-ecru.vercel.app/"
+        />
+      </Card>
+
+      <Card
+        style={{
+          width: "18rem",
+        }}
+      >
+        <CardPortfolio
+          imgUrl={`${basePath}/rbtumbl.png`}
+          altText="rb nails"
+          title="RB Nail Design"
+          subTitle=" Rafa Bach Nails"
+          description="Nesse Projeto foi usado Next.Js, tailwind, Sass, typescript e bootstrap com reactstrap"
+          url="https://rafabach-nails.vercel.app/"
+        />
+      </Card>
+
+      <Card
+        style={{
+          width: "18rem",
+        }}
+      >
+        <CardPortfolio
+          imgUrl={`${basePath}/khtumbl.png`}
+          altText="rb nails"
+          title="RB Nail Design"
+          subTitle=" Rafa Bach Nails"
+          description="Nesse Projeto foi usado Next.Js, tailwind, Sass, typescript e bootstrap com reactstrap"
+          url="https://n-delta-lake.vercel.app/"
+        />
+      </Card>
+
+      <Card
+        style={{
+          width: "18rem",
+        }}
+      >
+        <CardPortfolio
+          imgUrl={`${basePath}/vartumbl.png`}
+          altText="rb nails"
+          title="RB Nail Design"
+          subTitle=" Rafa Bach Nails"
+          description="Nesse Projeto foi usado Next.Js, tailwind, Sass, typescript e bootstrap com reactstrap"
+          url="https://varify.vercel.app/"
+        />
+      </Card>
+    </ListGroup>
   );
 }
