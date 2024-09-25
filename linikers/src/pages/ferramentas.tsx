44673465caa2d51738b32cf57e6943dc2c8f0f88
linikers <linikers@hotmail.com>
2024-09-25 13:58:16 -0300

    animationBaseFerramentas

--- linikers/src/pages/ferramentas.tsx
+++ linikers/src/pages/ferramentas.tsx
@@ -1,40 +1,102 @@
 import MenuUser from "@/components/menu";
-import { Container, Box, Typography, Grid2 } from "@mui/material";
+// import { Container, Box, keyframes } from "@mui/material";
 import { motion } from "framer-motion";
-import { Css, GitHub, Html, Javascript } from '@mui/icons-material'; 
+import { DiJsBadge } from "react-icons/di/index.js";
+import { DiCss3 } from "react-icons/di/index.js";
+import { DiLinux } from "react-icons/di/index.js";
+import { TbBrandTypescript } from "react-icons/tb/index.js";
+import { DiBootstrap } from "react-icons/di/index.js";
+import { DiDocker } from "react-icons/di/index.js";
+import { DiGithub } from "react-icons/di/index.js";
+import { DiGnu } from "react-icons/di/index.js";
+import { DiMagento } from "react-icons/di/index.js";
+import { DiMongodb } from "react-icons/di/index.js";
+import { DiSass } from "react-icons/di/index.js";
+import { DiPostgresql } from "react-icons/di/index.js";
+import { DiReact } from "react-icons/di/index.js";
+import { DiHtml5 } from "react-icons/di/index.js";
+import { DiScrum } from "react-icons/di/index.js";
+import { TbBrandNextjs } from "react-icons/tb/index.js";
+import { Box, Container, keyframes } from "@chakra-ui/react";
 
 const ferramentas = [
-  { icon: <Html />, label: "HTML5" },
-  { icon: <Css />, label: "CSS3" },
-  { icon: <Javascript />, label: "JavaScript" },
-  // { icon: <React />, label: "React" },
-  // { icon: <TypeScript />, label: "TypeScript" },
-  // { icon: <ReactIcon />, label: "Next" },
-  // { icon: <Bootstrap />, label: "Bootstrap" },
-  { icon: <GitHub />, label: "Git" },
+  { icon: <DiJsBadge size={45} />},
+  { icon: <TbBrandTypescript size={45} />},
+  { icon: <TbBrandNextjs size={45} /> },
+  { icon: <DiDocker size={45} /> },
+  { icon: <DiGithub size={45} /> },
+  { icon: <DiHtml5 size={45} /> },
+  { icon: <DiScrum size={45} /> },
+  { icon: <DiReact size={45} /> },
+  { icon: <DiMagento size={45} /> },
+  { icon: <DiCss3 size={45} /> },
+  { icon: <DiGnu size={45} /> },
+  { icon: <DiLinux size={45} /> },
+  { icon: <DiBootstrap size={45} /> },
+  { icon: <DiMongodb size={45} /> },
+  { icon: <DiPostgresql size={45} /> },
+  { icon: <DiSass size={45} /> },
 ];
+
+const scrollAnimation = keyframes`
+  0% { transform: translateX(0); }
+  // 50% { transform: translateX(-50%); }
+  100% { transform: translateX(-100%); }
+  // 50% { transform: translateX(50%); }
+  // 25% { transform: translateX(25%); }
+  // 0% { transform: translateX(0); }
+`
+const ScrollContainer = motion(Box);
 
 export default function Ferramentas() {
   return (
-    <Container sx={{ height: '100vh', bgcolor: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <MenuUser />
-      <Grid2 container spacing={2} sx={{ maxWidth: 400, marginTop: 2 }}>
+    <Container 
+      sx={{ 
+        justifyContent: 'center', 
+        flexDirection: 'column', 
+        alignItems: 'center',
+        bgcolor: '#d4d0c4', 
+        display: 'flex', 
+        height: '100vh',
+        overflow: 'hidden',
+        width: '100%',
+      }}>
+        <MenuUser />
+      <Box
+        sx={{
+          display: 'flex',
+          whiteSpace: 'nowrap',
+          flexDirection: 'row',
+          overflow: 'hidden',
+          width: '200%',
+          // justifyContent: 'center',
+          padding: '20px 0',
+          animation: `${scrollAnimation} 18s linear infinite`,
+        }}
+      >
         {ferramentas.map((ferramenta, index) => (
-          <Grid2 key={index}>
-            <Box display="flex" alignItems="center" justifyContent="space-around" color="white">
+            <Box key={index} display='inline-block' margin='0 20px' color="secondary">
               <motion.div
-                initial={{ filter: "blur(0px)" }}
-                whileHover={{ filter: "blur(4px)" }}
-                whileTap={{ filter: "blur(8px)" }}
-                style={{ display: "inline-block", transformOrigin: "center" }}
+              initial={{ scale: 1 }}
+              whileHover={{ scale: 1.2 }}
+              style={{ display: "inline-block", transformOrigin: "center" }}
               >
                 {ferramenta.icon}
               </motion.div>
-              <Typography variant="h6">{ferramenta.label}</Typography>
             </Box>
-          </Grid2>
         ))}
-      </Grid2>
+        {ferramentas.map((ferramenta, index) => (
+          <Box key={index + ferramentas.length} display="inline-block" margin="0 20px" color="secondary">
+            <motion.div
+              initial={{ scale: 1 }}
+              whileHover={{ scale: 1.2 }}
+              style={{ display: "inline-block", transformOrigin: "center" }}
+            >
+              {ferramenta.icon}
+            </motion.div>
+          </Box>
+        ))}
+      </Box>
     </Container>
   );
 }
