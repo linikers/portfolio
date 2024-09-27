225030ff3b8c6e2fd99248574bbfa2f0a65929e8
linikers <linikers@hotmail.com>
2024-09-27 10:53:16 -0300

    SEO conf

--- linikers/src/pages/_document.jsx
+++ linikers/src/pages/_document.jsx
@@ -10,9 +10,10 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           />
+          <title>LinikerS web Developer</title>
           <meta
-            name="description"
-            content="LinikerS Dev desenvolvedor Fron-End Maringá-Pr "
+            name="LinikerS webDeveloper"
+            content="LinikerS Dev desenvolvedor Fron-End && Back-End"
           />
         </Head>
         <body>
