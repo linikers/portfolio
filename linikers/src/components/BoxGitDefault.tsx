364eaa0d2241db1a01d60d999f9872e44d2b1e7d
linikers <linikers@hotmail.com>
2024-10-03 13:42:48 -0300

    ok ok

--- linikers/src/components/BoxGitDefault.tsx
+++ linikers/src/components/BoxGitDefault.tsx
@@ -1,12 +1,6 @@
 import { Box } from "@mui/material";
-import Image from "next/image";
-// import userIcon from "https://github-readme-stats.vercel.app/api?username=linikers&show_icons=true&theme=merko";
-// import langIcon from "https://github-readme-stats.vercel.app/api/top-langs/?username=linikers&langs_count=8&layout=compact&theme=merko";
 
 export default function BoxGitDefault() {
-    const userIcon = "https://github-readme-stats.vercel.app/api?username=linikers&show_icons=true&theme=merko";
-    const langIcon = "https://github-readme-stats.vercel.app/api/top-langs/?username=linikers&langs_count=8&layout=compact&theme=merko";
-  
 
     return (
         <Box 
@@ -21,31 +15,17 @@ export default function BoxGitDefault() {
           }}
         >
           <a href="https://github.com/linikers/github-readme-stats">
-            <Image 
-                src={langIcon}
-                alt="Top langs"
-                width={400}
-                height={200}
-                style={{ marginBottom: "10px" }}
-            />
-          {/* <img
+          <img
             src="https://github-readme-stats.vercel.app/api/top-langs/?username=linikers&langs_count=8&layout=compact&theme=merko"
             alt="Top Langs"
             style={{ width: "100%", maxWidth: "400px", marginBottom: "10px" }}
-          /> */}
+          />
         </a>
-        <Image 
-            src={userIcon}
-            alt="Liniker's GitHub stats"
-            width={400}
-            height={200}
-            style={{ width: "100%" }}
-        />
-        {/* <img
+        <img
           src="https://github-readme-stats.vercel.app/api?username=linikers&show_icons=true&theme=merko"
           alt="Liniker's GitHub stats"
           style={{ width: "100%", maxWidth: "400px" }}
-        /> */}
+        />
         </Box>
     )
 } 
