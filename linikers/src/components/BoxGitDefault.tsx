49933094165f45a1bc0da17ab1bd4a85c6251fed
linikers <linikers@hotmail.com>
2024-10-03 10:20:09 -0300

    debug - load fast

--- linikers/src/components/BoxGitDefault.tsx
+++ linikers/src/components/BoxGitDefault.tsx
@@ -1,6 +1,12 @@
 import { Box } from "@mui/material";
+import Image from "next/image";
+// import userIcon from "https://github-readme-stats.vercel.app/api?username=linikers&show_icons=true&theme=merko";
+// import langIcon from "https://github-readme-stats.vercel.app/api/top-langs/?username=linikers&langs_count=8&layout=compact&theme=merko";
 
 export default function BoxGitDefault() {
+    const userIcon = "https://github-readme-stats.vercel.app/api?username=linikers&show_icons=true&theme=merko";
+    const langIcon = "https://github-readme-stats.vercel.app/api/top-langs/?username=linikers&langs_count=8&layout=compact&theme=merko";
+  
 
     return (
         <Box 
@@ -15,17 +21,31 @@ export default function BoxGitDefault() {
           }}
         >
           <a href="https://github.com/linikers/github-readme-stats">
-          <img
+            <Image 
+                src={langIcon}
+                alt="Top langs"
+                width={400}
+                height={200}
+                style={{ marginBottom: "10px" }}
+            />
+          {/* <img
             src="https://github-readme-stats.vercel.app/api/top-langs/?username=linikers&langs_count=8&layout=compact&theme=merko"
             alt="Top Langs"
             style={{ width: "100%", maxWidth: "400px", marginBottom: "10px" }}
-          />
+          /> */}
         </a>
-        <img
+        <Image 
+            src={userIcon}
+            alt="Liniker's GitHub stats"
+            width={400}
+            height={200}
+            style={{ width: "100%" }}
+        />
+        {/* <img
           src="https://github-readme-stats.vercel.app/api?username=linikers&show_icons=true&theme=merko"
           alt="Liniker's GitHub stats"
           style={{ width: "100%", maxWidth: "400px" }}
-        />
+        /> */}
         </Box>
     )
 } 
