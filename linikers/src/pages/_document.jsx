6a8808dd25dd43995a98c95e4e9c02d182764e74
linikers <linikers@hotmail.com>
2024-10-04 11:48:57 -0300

    ...

--- linikers/src/pages/_document.jsx
+++ linikers/src/pages/_document.jsx
@@ -10,12 +10,12 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           />
-          <title>LinikerS web Developer</title>
           <meta
             name="LinikerS webDeveloper"
             content="LinikerS Dev desenvolvedor Fron-End && Back-End"
-          />
+            />
         </Head>
+        <title>LinikerS web Developer</title>
         <body>
           <Main />
           <NextScript />
