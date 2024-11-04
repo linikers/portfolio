718b66c38a4edb9decd5bf9a2957962abfc38751
linikers <linikers@hotmail.com>
2024-11-04 16:53:53 -0300

    instalações e criações de login e pré teste de autenticação

---
+++ linikers/src/pages/admin/dashboard/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Container, Typography } from "@mui/material";
+
+export default function Dashboard() {
+
+    return (
+        <Container>
+            <Typography>DashBoard Admin</Typography>
+        </Container>
+    )
+}
