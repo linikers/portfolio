6b4b7e96caceb113b47da8f797fbf1e4c05358fe
linikers <linikers@hotmail.com>
2024-11-06 15:15:48 -0300

    login admin configurado

--- linikers/src/pages/admin/dashboard/dashboard.tsx
+++ linikers/src/pages/admin/dashboard/dashboard.tsx
@@ -1,9 +1,11 @@
-import { Container, Typography } from "@mui/material";
+import { Avatar, Card, Container, Typography } from "@mui/material";
+
 
 export default function Dashboard() {
 
     return (
-        <Container>
+        <Container sx={{ mt: 4 }}>
+
             <Typography>DashBoard Admin</Typography>
         </Container>
     )
--- linikers/src/pages/admin/dashboard/index.tsx
+++ linikers/src/pages/admin/dashboard/index.tsx
@@ -1,11 +1,62 @@
-import { Grid2, Typography } from "@mui/material";
-
+import { Avatar, Box, Card, CardContent, Container, Grid2, Typography } from "@mui/material";
+import ava from "../../../../public/next.svg"
 export default function AdminPage() {
+
     return (
-        <Grid2>
-            <Typography>
-                Index Adm
-            </Typography>
-        </Grid2>
+        <Container sx={{ mt: 4 }}>
+            <Card
+                sx={{
+                    p: 2,
+                    mb: 4,
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar 
+                    alt="adm img"
+                    src={ava}
+                    sx={{
+                        width: 100,
+                        height: 100,
+                        mr: 2,
+                    }}
+                />
+                <Box>
+                    <Typography variant="h5">Seja bem vindo</Typography>
+                    <Typography variant="subtitle1">Admin</Typography>
+                </Box>
+            </Card>
+
+            <Grid2 container spacing={3}>
+                <Grid2 sx={{
+                    display: 'flex',
+                }}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6">Ultimos Tweets</Typography>
+                            <Typography variant="h4">421</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6">Ultimos Posts Linkedin</Typography>
+                            <Typography variant="h4">008</Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6">Ultimos PR</Typography>
+                            <Typography variant="h4">802</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid2>
+            </Grid2>
+            <Box>
+                <Typography>Projetos distribuidos</Typography>
+            </Box>
+            {/* <Typography>Index Admin</Typography> */}
+        </Container>
     )
 }
