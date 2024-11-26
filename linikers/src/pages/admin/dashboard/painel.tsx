d2da5a9ddb63adbcb0565a84ecd0224441b23839
linikers <linikers@hotmail.com>
2024-11-26 11:15:48 -0300

    fix btc

--- linikers/src/pages/admin/dashboard/painel.tsx
+++ linikers/src/pages/admin/dashboard/painel.tsx
@@ -7,9 +7,9 @@ import axios from "axios";
 import { CurrencyBitcoin } from "@mui/icons-material";
 
 interface IExchangeRates {
-    bitcoin: string | null;
-    usd: string | null;
-    eur: string | null;
+    bitcoin: { bid: string | null; ask: string | null };
+    usd: {bid: string | null; ask: string | null};
+    eur: {bid: string | null; ask: string | null};
 }
 export default function Painel() {
     // chaves google
@@ -17,37 +17,44 @@ export default function Painel() {
     // const clientId = '1410261414-gu4bmncrqgmi6c6cukiguvoutn3no32k.apps.googleusercontent.com';
 
     // function CoinData() {
-        const [exchangeRates, setExchangeRates] = useState<IExchangeRates>({ bitcoin: null, usd: null, eur: null});
+        const [exchangeRates, setExchangeRates] = useState<IExchangeRates>({
+            bitcoin: { bid: null, ask: null },
+            usd: { bid: null, ask: null },
+            eur: {bid: null, ask: null },
+        });
 
         useEffect(() => {
-            const fetchBitcoinPrice = async () => {
-                try {
-                    const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
-                    setExchangeRates(prevRates => ({
-                        ...prevRates,
-                        bitcoin: response.data.bpi.USD.rate,
-                    }));
-
-                } catch (error) {
-                    console.error('Erro no fetch bitcoin', error)
-                }
-            };
             const fetchExchangeRates = async () => {
                 try {
-                    const response = await axios.get('https://economia.awesomeapi.com.br/last/USD-BRL');
-                    setExchangeRates(prevRates =>({
-                        ...prevRates,
-                        usd: response.data.USDBRL
-                        // usd: response.data.bpi.USD,
-                        // eur: response.data.bpi.EUR,
-                    }))
+                    const response = await axios.get("https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL");
+                    
+                    const { USDBRL, EURBRL, BTCBRL } = response.data;
+
+                    setExchangeRates({
+                        bitcoin: {bid: BTCBRL, ask: BTCBRL },
+                        usd: { bid: USDBRL, ask: USDBRL },
+                        eur: { bid: EURBRL, ask: EURBRL },
+                    })
                 } catch (error) {
-                    console.error('Erro ao consultas moedas');
+                    console.error('Erro no fetch das moedas', error)
                 }
-            }
+            };
+            // const fetchExchangeRates = async () => {
+            //     try {
+            //         const response = await axios.get('https://economia.awesomeapi.com.br/last/USD-BRL');
+            //         setExchangeRates(prevRates =>({
+            //             ...prevRates,
+            //             usd: response.data.USDBRL
+            //             // usd: response.data.bpi.USD,
+            //             // eur: response.data.bpi.EUR,
+            //         }))
+            //     } catch (error) {
+            //         console.error('Erro ao consultas moedas');
+            //     }
+            // }
 
 
-            fetchBitcoinPrice();
+            // fetchBitcoinPrice();
             fetchExchangeRates();
         }, [])
     // }
@@ -55,29 +62,50 @@ export default function Painel() {
     return (
         <Container sx={{ mt: 4 }}>
 
-            <Typography>DashBoard Admin</Typography>
-            <Box>
+            <Typography variant="h4" gutterBottom>
+                DashBoard Admin
+            </Typography>
+            <Box
+                display="flex"
+                gap={2}
+                flexWrap="wrap"
+            >
                 <Card>
                     <CardContent>
                         <CurrencyBitcoin />
-                        <Typography>Bitcoin</Typography>
-                        ${exchangeRates.bitcoin}
+                        <Typography variant="h6">Bitcoin</Typography>
+                        <Typography>Compra: R$
+                            {exchangeRates.bitcoin.bid}
+                        </Typography>
+                        <Typography>Venda: R$
+                            {exchangeRates.bitcoin.ask}
+                        </Typography>
                     </CardContent>
                 </Card>
 
                 <Card>
                     <CardContent>
                         <CurrencyBitcoin />
-                        <Typography>Dolar Americano</Typography>
-                        ${exchangeRates.usd}
+                        <Typography variant="h6">Dolar Americano</Typography>
+                        <Typography>Compra: R$
+                            {exchangeRates.usd.bid}
+                        </Typography>
+                        <Typography>Venda: R$
+                            {exchangeRates.usd.ask}
+                        </Typography>
                     </CardContent>
                 </Card>
 
                 <Card>
                     <CardContent>
                         <CurrencyBitcoin />
-                        <Typography>Euro</Typography>
-                        ${exchangeRates.eur}
+                        <Typography variant="h6">Euro</Typography>
+                        <Typography>Compra: R$
+                            {exchangeRates.eur.bid}
+                        </Typography>
+                        <Typography>Venda: R$
+                            {exchangeRates.eur.ask}
+                        </Typography>
                     </CardContent>
                 </Card>
             </Box>
