ac9b2bde4774e0e709f758a63ae7e2b162fceb9c
linikers <linikers@hotmail.com>
2024-12-18 10:19:22 -0300

    comentando nextauth para teste do firebase

--- linikers/firebase.config.ts
+++ linikers/firebase.config.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCjo2qckQOdWe9JDPwqxNTBvhpnHRzjbFM",
-  authDomain: "portfoliolinikers.firebaseapp.com",
-  projectId: "portfoliolinikers",
-  storageBucket: "portfoliolinikers.firebasestorage.app",
-  messagingSenderId: "537302382611",
-  appId: "1:537302382611:web:40cd9a2390bf0ca76c27e6",
-  measurementId: "G-DGMR5208SP"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCjo2qckQOdWe9JDPwqxNTBvhpnHRzjbFM",
+//   authDomain: "portfoliolinikers.firebaseapp.com",
+//   projectId: "portfoliolinikers",
+//   storageBucket: "portfoliolinikers.firebasestorage.app",
+//   messagingSenderId: "537302382611",
+//   appId: "1:537302382611:web:40cd9a2390bf0ca76c27e6",
+//   measurementId: "G-DGMR5208SP"
+// };
 
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+// export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
