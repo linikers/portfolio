d04198b3edb87c43d82928c7e78b8fcdc09d67dc
linikers <linikers@hotmail.com>
2024-12-18 10:49:44 -0300

    teste

--- linikers/src/pages/_app.jsx
+++ linikers/src/pages/_app.jsx
@@ -1,11 +1,14 @@
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.scss";
 import "./types/global.d.ts";
 
+// export default function MyApp({ Component, pageProps }) {
+//   return (
+//     <SessionProvider session={pageProps.session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//       )
+// }
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-      )
+  return <Component {...pageProps} />;
 }
