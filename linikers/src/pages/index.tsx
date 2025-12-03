5576cf7b8bad7868acd09196b0496523cab95ca0
linikers <linikers@hotmail.com>
2025-12-03 15:29:46 -0300

    .

--- linikers/src/pages/index.tsx
+++ linikers/src/pages/index.tsx
@@ -1,24 +1,23 @@
-import BackgroundRoom from "./components/backgroundDesk/backgroundRoom";
 import Monitor from "./components/backgroundDesk/Monitor";
 
 export default function Home() {
   return (
     <div
-      className="home-root"
+      className="room-container"
       style={{
         position: "relative",
         width: "100vw",
         height: "100vh",
-        overflow: "hidden", // CRÍTICO: corta as laterais em telas menores
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#000",
+        overflow: "hidden",
+        backgroundImage: "url('/assets/bgroomhd.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        imageRendering: "pixelated",
       }}
     >
-      <BackgroundRoom>
-        <Monitor />
-      </BackgroundRoom>
+      {/* Monitor CRT (direita) */}
+      <Monitor />
     </div>
   );
 }
