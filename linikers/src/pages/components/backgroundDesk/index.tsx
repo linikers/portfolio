932b1272c2ffc4889dd4c7eb51defea7f75d733d
linikers <linikers@hotmail.com>
2025-12-03 15:48:55 -0300

    teste build

--- linikers/src/pages/components/backgroundDesk/index.tsx
+++ linikers/src/pages/components/backgroundDesk/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import BackgroundDesk from "./backgroundDesk";
 import Monitor from "./Monitor";
 
-export default function DeskScene() {
+export default function DeskScene({
+  children,
+}: {
+  children?: React.ReactNode;
+}) {
   return (
     <BackgroundDesk>
       <Monitor />
